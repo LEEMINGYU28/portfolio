918e1a018577aee05f310643fa3cb0bcf9ad0e9f
LEEMINGYU28 <stargene250@gmail.com>
2023-12-21 11:59:15 +0900

    add

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.getElementById("modal-box").onclick = (e) => {
 
 const projectArr = {
   firstProject: { details: "무슨 사이트를 할까 고민하다 클론코딩 보다는 직접 처음부터 해보자는 의미에서 앱으로만 만들어져 있는걸 웹에 만들어보자는 의미에서 시작" },
-  secondProject: { details: "랜덤으로 선정된 3명과 팀을 이뤄서 프로젝트 진행<br>팀장:이민규 팀원:김남균,박정완<br> 예약기능등 여러가지 기능이 있는 camping사이트를 조원들과의 회의를 통해 선정해서 진행" },
+  secondProject: { details: "랜덤으로 선정된 3명과 팀을 이뤄서 프로젝트 진행<br>팀장:이민규 팀원:김남균,박정완<br> 예약기능등 여러가지 기능이 있는 camping사이트를 조원들과의 회의를 통해 선정해서 진행 <br><a href='./file/camping.pdf' target='blink'><strong>상세문서</strong></a><br><br><a href='https://github.com/LEEMINGYU28/Camping' target='blink'><strong>깃허브</strong></a>" },
 };
 
 [...document.getElementsByClassName("project-info")].forEach((i) => {
