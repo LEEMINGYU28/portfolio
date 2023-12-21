dec2871be8f039008005b9e28c09da250e5ccfe7
LEEMINGYU28 <stargene250@gmail.com>
2023-12-21 10:10:51 +0900

    js add

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ document.getElementById("modal-box").onclick = (e) => {
 };
 
 const projectArr = {
-  firstProject: { details: "어쩌구 저쩌구~" },
-  secondProject: { details: "어쩌구2 저쩌구~2" },
+  firstProject: { details: "무슨 사이트를 할까 고민하다 클론코딩 보다는 직접 처음부터 해보자는 의미에서 앱으로만 만들어져 있는걸 웹에 만들어보자는 의미에서 시작" },
+  secondProject: { details: "랜덤으로 선정된 3명과 팀을 이뤄서 프로젝트 진행<br>팀장:이민규 팀원:김남균,박정완<br> 예약기능등 여러가지 기능이 있는 camping사이트를 조원들과의 회의를 통해 선정해서 진행" },
 };
 
 [...document.getElementsByClassName("project-info")].forEach((i) => {
